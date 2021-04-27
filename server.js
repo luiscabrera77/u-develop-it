6fac424412aa118183fc67c68de8fc8d171507ee
Luis <luis.cabrera@gmail.com>
2021-04-27 17:27:55 -0500

    queries

--- server.js
+++ server.js
@@ -26,14 +26,43 @@ app.get('/', (req, res) => {
   });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// GET all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
